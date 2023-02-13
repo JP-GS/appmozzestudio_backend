3dbba9504f6903c4337abbbaad005d2a398cb672
JP-GS <juanpgordillo89@gmail.com>
2023-02-13 08:14:35 -0300

    Usuario terminado, registro, login, reset pass

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,6 @@
 import Usuario from "../models/Usuario.js";
+import generarJWT from "../helpers/generarJWT.js";
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req, res) => {
     const { email } = req.body;
@@ -11,8 +13,8 @@ const registrar = async (req, res) => {
 
     try {
         const usuario = new Usuario(req.body);
-        const usuarioGuardado = await usuario.save()
-        res.json(usuarioGuardado);
+        await usuario.save()
+        res.json({ msg: 'Usuario guardado correctamente'});
 
     } catch (error) {
         console.log(error);
@@ -20,7 +22,8 @@ const registrar = async (req, res) => {
 };
 
 const perfil = (req, res) => {
-    res.json({msg: 'Mostrando perfil'});
+    const { usuario } = req;
+    return res.json({ perfil: usuario });
 }
 
 const confirmar = async (req, res) => {
@@ -38,9 +41,82 @@ const confirmar = async (req, res) => {
         await usuarioConfirmar.save();
     } catch (error) {
         console.log(error);
+    };
+};
+
+const autenticar = async (req, res) => {
+    const { email, password } = req.body;
+
+    const usuario = await Usuario.findOne({ email });
+    //Revisa si el usuario existe
+    if(!usuario) {
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({msg: error.message});
+    };   
+    //Revisa si el usuario está confirmado
+    if(!usuario.confirmado) {
+        const error = new Error('Tu cuenta no ha sido confirmada');
+        return res.status(403).json({ msg: error.message});
+    };
+    //Revisa si el password es correcto
+    if(await usuario.comprobarPassword(password)) {
+        //Autenticar el usuario
+        res.json({ token: generarJWT(usuario._id) });
+    } else {
+        const error = new Error('Password Incorrecto');
+        res.status(401).json({ msg: error.message });
     }
-}
+};
+
+const olvidePassword = async (req,res) => {
+    const {email} = req.body;
+
+    const existeUsuario = await Usuario.findOne({email});
+    if(!existeUsuario) {
+        const error = new Error('Usuario no encontrado');
+        return res.status(400).json({msg: error.message});
+    };
+
+    try {
+        existeUsuario.token = generarId();
+        await existeUsuario.save();
+        res.json({msg: 'Hemos enviado un email a tu casilla de correo, revisa y verifica tu cuenta'});
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const comprobarToken = async (req,res) => {
+    const { token } = req.params;
+
+    const tokenValido = await Usuario.findOne({ token });
+    if(tokenValido) {
+        res.json({msg: 'Token válido! El usuario tiene permiso para cambiar la contraseña'})
+    } else {
+        const error = new Error('Token no válido');
+        return res.status(400).json({ msg: error.message });
+    }
+};
+
+const nuevoPassword = async (req,res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    const usuario = await Usuario.findOne({ token });
+    if(!usuario) {
+        const error = new Error('Hubo un error');
+        return res.status(400).json({msg: error.message});
+    };
+     try {
+        usuario.token = '';
+        usuario.password = password;
+        await usuario.save();
+        return res.status(200).json({msg: 'Contraseña reestablecida correctamente'});
+     } catch (error) {
+        console.log(error);
+     }
+};
 
 export {
-    registrar, perfil, confirmar
-}
+    registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword
+};
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,16 @@
 import  express from "express";
-import { registrar, perfil, confirmar } from "../controllers/usuarioController.js";
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/usuarioController.js";
+import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
+//área pública
 router.post('/', registrar);
-router.get('/perfil', perfil);
 router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+//áreaa privada
+router.get('/perfil', checkAuth, perfil);
 
 export default router;
